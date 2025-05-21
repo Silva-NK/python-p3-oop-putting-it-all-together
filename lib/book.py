5b0e85bf18df57bb433a4e9b66de49f772fda1f6
Silva-NK <kiagonsuzanne@gmail.com>
2025-05-21 13:20:34 +0300

    Builds classes Book and Shoe to achieve desired goals.

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,35 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def title(self):
+        return self._title
     
+    @title.setter
+    def title(self, title):
+        self._title = title
+
+
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, page_count):
+        if not isinstance (page_count, int):
+            print("page_count must be an integer")
+        self._page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
         
+    
+book1 = Book("Hamlet", "432")
+
+print(book1.title)
+print(book1.page_count)
+book1.turn_page()
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,37 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = None
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, brand):
+        self._brand = brand
+
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            print("size must be an integer")
+        self._size = size
+    
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+shoe1 = Shoe("Manolo Blahnik", "43")
+
+print(shoe1.brand)
+print(shoe1.size)
+shoe1.cobble()
